8c41c821d4611137ebf65da509e4bd6041080041
Sam G. Zun <zunsamuel97@gmail.com>
2018-07-17 16:58:51 -0500

    Deploy Attempt

---
+++ keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    app_id: process.env.PJ_TWO_APP_ID,
+    app_key: process.env.PJ_TWO_APP_KEY
+}
--- routes/mealMaster.js
+++ routes/mealMaster.js
@@ -2,13 +2,17 @@ const express = require("express");
 const router = express.Router();
 const mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: null,
-  database: "whats_for_dinner_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: null,
+    database: "whats_for_dinner_db"
+  });
+}
 
 router.get("/", (req, res) => {
   connection.query("SELECT meal FROM meal_master;", (err, response) => {
