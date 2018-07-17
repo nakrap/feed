5e2ae126125bd16ef2a88d250ec02792722f78e6
Nick Akrap <nakrap@gmail.com>
2018-07-17 15:58:02 -0500

    added jawsDB and connected to heroku

--- routes/mealMaster.js
+++ routes/mealMaster.js
@@ -2,13 +2,21 @@ const express = require("express");
 const router = express.Router();
 const mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: null,
-  database: "whats_for_dinner_db"
-});
+var connection;
+
+// Set up JawsDB with Heroku.
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: null,
+        database: "whats_for_dinner_db"
+    });
+}
 
 router.get("/", (req, res) => {
   connection.query("SELECT meal FROM meal_master;", (err, response) => {
